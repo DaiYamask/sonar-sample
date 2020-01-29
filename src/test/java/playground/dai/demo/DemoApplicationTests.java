f4911aeff0bf26f62c5b23f278dfb419a53dd6d0
dai <dai@tagbangers.co.jp>
2020-01-29 23:49:39 +0900

    Initial commit

---
+++ src/test/java/playground/dai/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package playground.dai.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
